49ee76ab90da73c7705787a772276ac0c3e5e46a
lhien11@gmail.com <trunghien11>
2018-03-13 13:37:08 -0700

    Route example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  {
+    path: 'user', children:
+      [
+        { path: '', pathMatch: 'full', redirectTo: 'list' },
+        {
+          path: 'list', component: UserListComponent, children: [
+            { path: 'detail/:name', component: UserComponent }
+          ]
+        },
+
+      ]
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    UserComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: false })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class UserComponent implements OnInit {
+  name: any;
+  sub: any;
+
+  constructor(private route:ActivatedRoute) { }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params =>{
+      this.name = params['name'];
+    })
+  }
+  
+
+}
